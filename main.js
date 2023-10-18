2db3637f1aabb607ce61ecaa874b8951d2b954b1
Vilson <khvilson@yandex.ru>
2023-10-19 01:57:30 +0400

    added thumbs

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-const swiper = new Swiper(".swiper", {});
+const mainSlider = new Swiper(".swiper", {
+	watchSlidesProgress: true,
+});
+
+const thumbsSlider = new Swiper(".thumbsSlider", {
+	direction: "vertical",
+	slidesPerView: 3,
+	centeredSlides: true,
+});
+
+mainSlider[0].controller.control = thumbsSlider;
+
+const sliderImage = document.querySelectorAll(".slider-image");
+const navigation = document.querySelector(".navigation");
+const info = document.querySelector(".info");
+
+// sliderImage.forEach((image) => {
+// 	image.addEventListener("mouseover", () => {
+// 		navigation.style.transform = "translateX(-50%) rotate(-20deg)";
+// 		info.style.transform = "translateY(50%)";
+// 	});
+
+// 	image.addEventListener("mouseout", () => {
+// 		navigation.style.transform = "translateX(0) rotate(-20deg)";
+// 		info.style.transform = "translateY(0)";
+// 	});
+// });
